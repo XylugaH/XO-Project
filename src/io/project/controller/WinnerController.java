442ddc6f503982ceccbacfe1bb95d39a27c69487
Андрей <@192.168.1.3>
2016-09-02 21:56:01 +0300

    refactoring field

--- src/io/project/controller/WinnerController.java
+++ src/io/project/controller/WinnerController.java
@@ -5,11 +5,10 @@ import io.project.model.Field;
 public class WinnerController {
 	
 	public void getWinner(final Field field){
-		for (int i = 0; i < field.getSize(); i++) {
-			for (int j = 0; j < field.getSize(); j++) {
-				
-			}
-		}
+
 		return;
 	}
+	
+	
+	
 }
--- src/io/project/model/Field.java
+++ src/io/project/model/Field.java
@@ -3,20 +3,29 @@ package io.project.model;
 import io.project.model.exceptions.InvalidPointException;
 
 public class Field {
-	private final int size;
+	private final int sizeX;
+	private final int sizeY;
 
 	private final Figure[][] figures;
 	
 		
-	public Field(final int size) {
-		this.size = size;
-		this.figures = new Figure[size][size];
+	public Field(final int sizeX, final int sizeY) {
+		this.sizeX = sizeX;
+		this.sizeY = sizeY;
+		this.figures = new Figure[sizeX][sizeY];
 	}
 
-	public int getSize(){
-		return size;
+
+	public int getSizeX() {
+		return sizeX;
 	}
-	
+
+
+	public int getSizeY() {
+		return sizeY;
+	}
+
+
 	public Figure getFigure(final Point point) throws InvalidPointException{
 		if (!checkPoint(point)){
 			throw new InvalidPointException();
@@ -34,10 +43,14 @@ public class Field {
 	}
 	
 	private boolean checkPoint(final Point point){
-		return chekCoordinate(point.getX()) && chekCoordinate(point.getY());
+		return chekCoordinateX(point.getX()) && chekCoordinateY(point.getY());
 	}
 	
-	private boolean chekCoordinate(final int coordinate){
-		return coordinate > 0 && coordinate <= size;
+	private boolean chekCoordinateX(final int coordinate){
+		return coordinate > 0 && coordinate <= sizeX;
 	}
+	
+	private boolean chekCoordinateY(final int coordinate){
+		return coordinate > 0 && coordinate <= sizeY;
+	}	
 }
