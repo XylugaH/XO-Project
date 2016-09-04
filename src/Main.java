158add937f906b082af41640021b3609b178f962
Андрей <@192.168.1.3>
2016-09-04 17:55:12 +0300

    Prerealise

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import io.project.controller.WinnerController;
+import io.project.model.Field;
+import io.project.model.Figure;
+import io.project.model.Game;
+import io.project.model.Player;
+import io.project.model.Point;
+import io.project.model.exceptions.InvalidPointException;
+import io.project.view.ConsoleView;
+
+public class Main {
+
+	public static void main(String[] args) throws InvalidPointException {
+		final String name1 = "Андрей";
+		final String name2 = "Таня";
+		final String nameGame = "Крестики-Нолики";
+		final Player[] players = new Player[] {new Player(name1, Figure.X), new Player(name2, Figure.O)};
+		final Field field = new Field(3, 3);
+		final Game game = new Game(field, players, nameGame);
+		final ConsoleView consoleView = new ConsoleView();
+		while(consoleView.move(game)){
+			consoleView.show(game);
+		}
+		
+	}
+
+}
--- src/io/project/view/ConsoleView.java
+++ src/io/project/view/ConsoleView.java
@@ -1,27 +1,74 @@
 package io.project.view;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import io.project.controller.CurrentMoveController;
+import io.project.controller.MoveController;
+import io.project.controller.WinnerController;
 import io.project.model.Field;
 import io.project.model.Figure;
 import io.project.model.Game;
 import io.project.model.Point;
+import io.project.model.exceptions.AlreadyOccupiedException;
 import io.project.model.exceptions.InvalidPointException;
 
 public class ConsoleView {
+	private final CurrentMoveController currentMoveController = new CurrentMoveController();
+	private final WinnerController winnerController = new WinnerController();
+	private final MoveController moveController = new MoveController();
 	
 	public void show(final Game game){
-		Field field = game.getField();
+		final Field field = game.getField();
+		System.out.format("Game name: %s\n", game.getName());
 		for (int y = 0; y < field.getSizeY(); y++) {
 			printLine(field, y);
 		}
+		printSeparator(game.getField());
 	}
 	
 
-	public void move(final Game game){
+	public boolean move(final Game game){
+		final Field field = game.getField(); 
+		final Figure figure = currentMoveController.currentMove(field);
+		final Figure winnerFigure = winnerController.getWinner(field);
+		if (winnerFigure != null){
+			System.out.format("Игра окончена. Победил %s", winnerFigure.getName());
+			return false;			
+		}
+		if (figure == null){
+			System.out.println("Игра окончена: Ничья!!!");
+			return false;
+		}
+		System.out.format("Ход игрока %s\n", figure.getName());
+		final Point point;
+		try{
+			 point = getPoint();
+		}catch (InputMismatchException e){
+			return true;
+		}
+		try {
+			moveController.applyFigure(point, field, figure);
+		} catch (AlreadyOccupiedException | InvalidPointException e) {
+			System.out.println("Введена неверная координата!!!");
+		}
 		
+		return true;
+	}
+	
+	private Point getPoint(){
+		return new Point(getCoordinate("X")-1, getCoordinate("Y")-1);
+	}
+	
+	private int getCoordinate(final String coordinate){
+		System.out.format("Пожалуйста введите координату: %s", coordinate);
+		final Scanner in = new Scanner(System.in);
+		return in.nextInt();
 	}
 	
 	private void printLine(final Field field, final int y) {
-		System.out.println('|');
+		printSeparator(field);
+		System.out.print('|');
 		for (int x = 0; x < field.getSizeX(); x++) {
 			final Figure figure;
 			try {
@@ -29,12 +76,17 @@ public class ConsoleView {
 			} catch (InvalidPointException e) {
 				throw new RuntimeException(e);
 			}
-			System.out.println(figure != null ? figure.getName() : " ");
-			System.out.println('|');
+			System.out.print(figure != null ? figure.getName() : " ");
+			System.out.print('|');
 		}
+		System.out.println();
 	}
 	
 	private void printSeparator(final Field field){
-		
+		final int countSeparators = field.getSizeX()*2+1;
+		for (int i = 0; i < countSeparators; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
 	}
 }
